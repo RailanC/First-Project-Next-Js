ce1f07f98efae842601b14dad2c80722638009bf
RailanC <@Railan>
2025-01-25 12:19:58 +0100

    Start of the project

---
+++ app/layout.tsx
@@ -0,0 +1,64 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import localFont from "next/dist/compiled/@next/font/dist/local";
+
+const workSans = localFont({
+    src: [
+        {
+            path: './fonts/WorkSans-Black.ttf',
+            weight: '900',
+            style: 'normal',
+        }, {
+            path: './fonts/WorkSans-ExtraBold.ttf',
+            weight: '800',
+            style: 'normal',
+        }, {
+            path: './fonts/WorkSans-Bold.ttf',
+            weight: '700',
+            style: 'normal',
+        }, {
+            path: './fonts/WorkSans-SemiBold.ttf',
+            weight: '600',
+            style: 'normal',
+        }, {
+            path: './fonts/WorkSans-Medium.ttf',
+            weight: '500',
+            style: 'normal',
+        }, {
+            path: './fonts/WorkSans-Regular.ttf',
+            weight: '400',
+            style: 'normal',
+        }, {
+            path: './fonts/WorkSans-Thin.ttf',
+            weight: '200',
+            style: 'normal',
+        }, {
+            path: './fonts/WorkSans-ExtraLight.ttf',
+            weight: '100',
+            style: 'normal',
+        },
+    ],
+    variable: '--font-work-sans',
+})
+
+export const metadata: Metadata = {
+  title: "Tutorial 5H",
+  description: "Learning Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={workSans.variable}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
