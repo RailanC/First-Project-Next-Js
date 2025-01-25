af26c0d262e436815e3b5b9d48997c4cb22e4533
RailanC <@Railan>
2025-01-25 18:15:33 +0100

    Components, Card Grid Creation of 1 component responsible to modulate a card. Creation of a ul, and a map from a post variable to create the cards, data manipulated locally. Changes in same configurations to remove the default limitations.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,21 @@
 import Image from "next/image";
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({searchParams}: {
     searchParams: Promise<{query?: string}>
 }) {
     const query = (await searchParams).query;
+    const posts = [{
+        _createAt: new Date(),
+        views: 55,
+        author: {_id: 1, name: "John Doe"},
+        _id: 1,
+        description: "This is a description",
+        image: "https://img.freepik.com/free-photo/cartoon-ai-robot-scene_23-2151658112.jpg",
+        category: "Robots",
+        title: "We Robots"
+    }]
   return (
       <>
           <section className={"pink_container"}>
@@ -15,6 +26,19 @@ export default async function Home({searchParams}: {
               </p>
 
               <SearchForm query={query} />
+          </section>
+          <section className="section_container">
+              <p className="text-30-semibold">
+                  {query ? `Search results for ${query}` : 'All Startups'}
+              </p>
+
+              <ul className="mt-7 card_grid">
+                  {posts?.length > 0 ? (
+                      posts.map((post: StartupCardType, index: number) => (
+                          <StartupCard key={post?._id} post={post} />
+                      ))
+                  ): (<p className="no-results">No Startups found.</p>)}
+              </ul>
           </section>
       </>
   );
