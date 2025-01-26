043d1057943974e39704c06ba887524ab20da260
RailanC <@Railan>
2025-01-26 15:58:11 +0100

    Fetching Data -Implementation of type generator using typegen of sanity to dynamically create the type of cards.

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -4,13 +4,26 @@ import {EyeIcon} from "lucide-react"
 import Link from "next/link";
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
-const StartupCard = ({ post }: { post: StartupTypeCard} )    => {
-    const { _createAt, image, description, _id, title, category, views, author:{_idAuthor, name} } = post;
+import {Author, Startup} from "@/sanity/types";
+
+export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
+
+const StartupCard = ({ post }: { post: StartupTypeCard } )=> {
+    const {
+        image,
+        _createdAt,
+        description,
+        _id,
+        title,
+        category,
+        views,
+        author
+    } = post;
     return (
         <li className="startup-card group">
             <div className="flex-between">
                 <p className="startup_card_date">
-                    {formatDate(_createAt)}
+                    {formatDate(_createdAt)}
                 </p>
 
                 <div className="flex gap-1.5">
@@ -20,14 +33,14 @@ const StartupCard = ({ post }: { post: StartupTypeCard} )    => {
             </div>
             <div className="flex-between mt-5 gap-5">
                 <div className="flex-1">
-                    <Link href={`/user/${_idAuthor}`}>
-                        <p className="text-16-medium line-clamp-1">{name}</p>
+                    <Link href={`/user/${author?._id}`}>
+                        <p className="text-16-medium line-clamp-1">{author?.name}</p>
                     </Link>
                     <Link href={`/startup/${_id}`}>
                         <h3 className="text-26-semibold line-clamp-1">{title}</h3>
                     </Link>
                 </div>
-                    <Link href={`/user/${_idAuthor}`}>
+                    <Link href={`/user/${author?._id}`}>
                         <Image src="https://placehold.co/48x48" width={48} height={48} alt="placeholder" className="rounded-full" />
                     </Link>
             </div>
@@ -37,7 +50,7 @@ const StartupCard = ({ post }: { post: StartupTypeCard} )    => {
             </Link>
 
             <div className="flex-between gap-3 mt-5">
-                <Link href={`/query/${category.toLowerCase()}`}>
+                <Link href={`/query/${category?.toLowerCase()}`}>
                     <p className="text-16-medium">{category}</p>
                 </Link>
                 <Button className="startup-card_btn">
